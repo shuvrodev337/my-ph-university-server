fa0150fba00b002d01864e24baa1ac79caa027e5
Shuvro Dev Mondal <shuvrodevmondal337@gmail.com>
2025-01-08 04:07:39 +0600

    added jwt basic setup

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,8 +2,10 @@
 
 import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/AppError';
+import jwt from 'jsonwebtoken';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import config from '../../config';
 
 const loginUser = async (payload: TLoginUser) => {
   const { id, password } = payload;
@@ -24,7 +26,32 @@ const loginUser = async (payload: TLoginUser) => {
     throw new AppError(StatusCodes.FORBIDDEN, 'Wrong password!');
   }
 
-  return {};
+  const jwtPayload = {
+    userId: user.id,
+    role: user.role,
+    // we will not sen sensitive data here, ex: password. we will send this payload, encrypted through accessToken
+  };
+
+  // create token and send to client
+  const accessToken = jwt.sign(
+    {
+      data: jwtPayload,
+    },
+    config.jwt_access_secret as string, //  secret from from server, not going purely, going as encrypted
+    { expiresIn: '10d' },
+  );
+
+  return {
+    accessToken,
+    needsPasswordChange: user?.needsPasswordChange, // when Admin creates an account for a faculty or student,
+    // it will initially set default password.that is why we need to pass this property for this account when logged in.
+    // then the user can know whether he needs password change or not.
+  };
 };
 
 export const AuthServices = { loginUser };
+/*
+how to generate random number for JWT_ACCESS_SECRET
+open =>NODE in terminal
+then =>require('crypto').randomBytes(32).toString('hex')
+*/
